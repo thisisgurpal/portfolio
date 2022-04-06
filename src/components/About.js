0eaf641aefebe914d324aab98f639695f199c583
Gurpal Gohler <gurpal.gohler@hotmail.co.uk>
2022-04-06 17:44:55 +0100

    correct spelling

--- src/components/About.js
+++ src/components/About.js
@@ -30,7 +30,7 @@ function About() {
     <Flex flexDirection='column' alignItems='center' minHeight='100vh'>
       <Text textAlign='center' className='title' fontSize='30px'>About Me</Text>
       <Text w='80%' mt='5'>I’m a Junior Full Stack Developer with a background in mathematics, analytics and art and design. I studied Financial Mathematics at university and soon after graduating I started a new role in data analytics. Working closely with other developers in this role and understanding the link between development and design intrigued me, I soon after completed the full time Software Engineering Immersive bootcamp at General Assembly. I’m passionate about continuing to enhance my software engineering skills to learn and grow as a developer in a dynamic environment.</Text>
-      <Text w='80%' mt='5' mb='5'>In my free time I like to make art. Sometimes I like to take an object and create new ways to in how you view it...</Text>
+      <Text w='80%' mt='5' mb='5'>In my free time I like to make art. Sometimes I like to take an object and create new ways in how you view it...</Text>
       <div className="App">
     <CarouselWrapper items={itemsV2} mode="gallery">
       <div className="imagev2 image1v2"></div>
